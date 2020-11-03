03525a23c526fe55ef65f8d8d0e47a48785b4d39
Rachel Moore <moorer@gcsnc.com>
2020-11-03 11:00:29 -0500

    added all the user input questions

--- index.js
+++ index.js
@@ -1,16 +1,153 @@
+const inquirer = require("inquirer");
+var fs = require("fs");
+var util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
 // array of questions for user
-const questions = [
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+        type: "input",
+        name: "gitname",
+        message: "What is your Git Hub profile address?",
+      },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
+    {
+      type: "input",
+      name: "projectName",
+      message: "What is your Project Name?",
+    },
+    {
+      type: "input",
+      name: "desc",
+      message: "Please write a short description of your project",
+    },
+    {
+      type: "input",
+      name: "user",
+      message: "What is the User Story?",
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      message: "What type of license should your project have?",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "NONE"],
+    },
+    {
+      type: "input",
+      name: "depend",
+      message: "What command should be run to install dependencies?",
+    },
+    {
+        type: "input",
+        name: "builders",
+        message: "Who contributed to the Project?",
+      },
+    {
+      type: "input",
+      name: "test",
+      message: "What command should be run to run tests?",
+    },
+    {
+      type: "input",
+      name: "repo",
+      message: "What does the user need to know about using the repo?",
+    },
+    {
+      type: "input",
+      name: "contrib",
+      message:
+        "What does the user need to know about contributing to the repo?",
+    },
+  ]);
+};
 
-];
+// function to generate markdown for README
 
-// function to write README file
-function writeToFile(fileName, data) {
+function generateMarkdown(answers) {
+  return `# ${answers.title}
+
+
+  # ${answers.projectName}
+
+  ![Licernse](https://img.shields.io/badge/License-${response.license}-blue.svg "License Badge")
+  
+
+
+    # Table of Contents
+
+    - [Description](#desc)
+    - [User Story](#user)
+    - [Installation](#depend)
+    - [Usage](#repo)
+    - [Contributors](#builders)
+    - [Contributing](#contrib)
+    - [Testing](#test)
+    - [License](#license)
+    
+
+## Description:
+    ${answers.desc}
+
+## User Story:
+    ${answers.user}
+
+## Installation:
+    ${answers.depend}
+
+## Usage:
+    ${answers.repo}
+
+## Contributors:
+    ${answers.builders}
+
+## Contributing:
+    ${answers.contrib}
+
+## Testing:
+    ${answers.test}
+
+## License:
+    ${answers.license}
+
+## Questions:
+For questions about Node File Generator, visit my GitHub page:
+    ${answers.gitname}
+  
+  ## You may also contact me via email at:
+  ${answers.email}
+  
+  ## License:
+  Click on the link to find information about the license
+  [License](https://opensource.org/licence/${answers.license})
+`;
 }
 
 // function to initialize program
-function init() {
 
-}
+promptUser()
+  .then((answers) => {
+    const readMe = generateMarkdown(answers);
+    return writeFileAsync("README.md", readMe);
+  })
+  .then(() => {
+    console.log("Successfully wrote to README.md");
+  })
+  .catch((err) => console.log(err));
 
-// function call to initialize program
-init();
+
+
+//   function call to initialize program
+  function init() 
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,81 +14,97 @@ const promptUser = () => {
       },
       {
         type: 'input',
-        name: 'location',
-        message: 'Where are you from?'
+        name: 'email',
+        message: 'What is your email address?'
       },
       {
         type: 'input',
-        name: 'hobby',
-        message: 'What is your favorite hobby?'
+        name: 'projectName',
+        message: 'What is your Project Name?'
       },
       {
         type: 'input',
-        name: 'food',
-        message: 'What is your favorite food?'
+        name: 'desc',
+        message: 'Please write a short description of your project'
       },
       {
         type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username'
+        name: 'license',
+        message: 'What type of license should your project have?'
+        // choices: 'MIT, APACHE 2.0, GPL 3.0 BSD 3, NONE'
       },
       {
         type: 'input',
-        name: 'linkedin',
-        message: 'Enter your LinkedIn URL.'
+        name: 'depend',
+        message: 'What command should be run to install dependencies?'
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?'
+        
+      },
+      {
+        type: 'input',
+        name: 'repo',
+        message: 'What does the user need to know about using the repo?'
+      },
+      {
+        type: 'input',
+        name: 'contrib',
+        message: 'What does the user need to know about contributing to the repo?'
       }
     ]);
   };
 
 
   // function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
 
-// `;
-// }
+module.exports = generateMarkdown;
+  
+const generateMarkdown(data) => {
+  return `# ${data.title}
 
-// module.exports = generateMarkdown;
+ `;
+ }
+
+//   <!doctype >
+//   <html lang="en">
+//     <head>
+//       <!-- Required meta tags -->
+//       <meta charset="utf-8">
+//       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   
-const generateHTML = answers => {
-  return `
-  <!doctype html>
-  <html lang="en">
-    <head>
-      <!-- Required meta tags -->
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  
-      <!-- Bootstrap CSS -->
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-      <link rel="stylesheet" href="style.css">
-      <title>Creating html with node.js</title>
-    </head>
-    <body>
+//       <!-- Bootstrap CSS -->
+//       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+//       <link rel="stylesheet" href="style.css">
+//       <title>Creating html with node.js</title>
+//     </head>
+//     <body>
         
-      <div class="jumbotron" id='app' >
-          <h1 class="display-4" >${answers.name}</h1>
-          <p class="lead">${answers.location}</p>
-          <h3 class="my-4">${answers.hobby}<button type="button" class="btn btn-secondary">Contact Me</button></h3>
+//       <div class="jumbotron" id='app' >
+//           <h1 class="display-4" >${answers.name}</h1>
+//           <p class="lead">${answers.location}</p>
+//           <h3 class="my-4">${answers.hobby}<button type="button" class="btn btn-secondary">Contact Me</button></h3>
   
-          <div class="github">${answers.github}</div>
-          <hr>
-          <div class="linkedIn">${answers.linkedin}</div>
+//           <div class="github">${answers.github}</div>
+//           <hr>
+//           <div class="linkedIn">${answers.linkedin}</div>
           
-        </div>
+//         </div>
       
-    </body>
-  </html>`;
-};
+//     </body>
+//   </html>`;
+// };
 
 
-promptUser()
-  .then(answers => {
-    const html = generateHTML(answers);
-    return writeFileAsync('index.html', html);
-  })
-  .then(() => {
-    console.log('Successfully wrote to index.html');
-  })
-  .catch(err => console.log(err));
+// promptUser()
+//   .then(data => {
+//     const html = generateHTML(answers);
+//     return writeFileAsync('index.html', html);
+//   })
+//   .then(() => {
+//     console.log('Successfully wrote to index.html');
+//   })
+//   .catch(err => console.log(err));
 
